cbc070a1f8bdec0d87d9461e5b3627a68a9bc899
pbaker321 <pete.baker111@gmail.com>
2021-04-29 14:33:35 +0000

    api prints to page. not working needs debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 
 let ingredients = document.getElementById("input").value;
 let button = document.getElementById("button");
-
+//let output = document.getElementById("meal")
+let html =""
 button.addEventListener('click', getRecipe)
 
 
@@ -12,9 +13,21 @@ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`)
     .then(result => { const mealList = result
         if(mealList.meals){
             mealList.meals.forEach(meal => {
-                console.log(meal)
+            html += `
+                <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+            ` ;                 
             });
-        }
+             document.getElementById("meal").innerHTML = html
+        };
+           
          })
     .catch(error => console.log('error', error))
 }
