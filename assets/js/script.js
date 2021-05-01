7666f6b964e350e1fd20a2db8299c2b42c939846
pbaker321 <pete.baker111@gmail.com>
2021-05-01 11:51:08 +0000

    fixed bug in api,

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,42 @@
 
-let ingredients = document.getElementById("input").value;
+
 let button = document.getElementById("button");
-//let output = document.getElementById("meal")
+let output = document.getElementById("meal")
 let html =""
 button.addEventListener('click', getRecipe)
 
 
 // API CALL
 function getRecipe(){
-fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`)
-    .then(response => response.json())
-    .then(result => { const mealList = result
-        if(mealList.meals){
-            mealList.meals.forEach(meal => {
-            html += `
-                <div class = "meal-item" data-id = "${meal.idMeal}">
-                        <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
-                        </div>
-                        <div class = "meal-name">
-                            <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
+    let ingredients = document.getElementById("ingredients").value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`)
+        .then(response => response.json())
+        .then(result => { const mealList = result
+            if(mealList.meals){
+                mealList.meals.forEach(meal => {
+                html += `
+                <div>
+                    <div class = "meal-img">
+                        <img src = "${meal.strMealThumb}" alt = "food">
                     </div>
-            ` ;                 
-            });
-             document.getElementById("meal").innerHTML = html
-        };
-           
-         })
-    .catch(error => console.log('error', error))
-}
+                    <div class = "meal-name">
+                        <h3>${meal.strMeal}</h3>
+                    </div>
+                </div>
+                ` ;                 
+                });
+                output.classList.remove('notFound');
+            } else{
+                html = "Sorry, we didn't find any meal!";
+                output.classList.add('notFound');
+            }
+
+            output.innerHTML = html;
+            })
+            
+            };
+
+
 
 
      
